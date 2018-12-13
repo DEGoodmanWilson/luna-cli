0eb268663719b4e6b9c1cdba6c32c75eb7c26b02
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-12-13 09:47:05 +0100

    Bump version to v1.0.1

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, CMake
 
 class LunaPackageConan(ConanFile):
     name = "luna-cli"
-    version = "1.0.0"
+    version = "1.0.1"
     url = "https://github.com/DEGoodmanWilson/luna-cli.git"
     license = "MIT"
 
